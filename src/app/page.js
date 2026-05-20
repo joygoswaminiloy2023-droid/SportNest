2708786d950f36c64e89963faede2e1b6856c6a1
Joy Goswami <joygoswaminiloy2023@gmail.com>
2026-05-20 10:39:06 +0600

    data fethcing issue

--- src/app/page.js
+++ src/app/page.js
@@ -1,53 +1,40 @@
+"use client";
 
-import Hero from "./Components/Header/Hero";
-import Cardfacility from "./Components/Header/Card.jsx/Cardfacility";
-import {motion} from "framer-motion"
-import WhyChooseUs from "./Components/Features/WhyChooseUs";
-import UpcomingEvents from "./Events/page";
+import { motion } from "framer-motion";
+import Cardfacility from "../Header/Card.jsx/Cardfacility";
 
-
-export default async function Home() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BETTER_AUTH_URL}/facility`,{cache:"no-cache"});
-  const data = await res.json();
-
+const FeaturedFacilities = ({ data }) => {
   return (
-    <>
-      <Hero />
-      
-
-      <div className="container mx-auto my-10">
-        
-        <h1 className="text-2xl font-bold text-center lg:text-left ml-10 ">Featured Facilities</h1>
-        
-  <motion.div
-  initial={{ opacity: 0, y: 60 }}
-  whileInView={{ opacity: 1, y: 0 }}
-  transition={{ duration: 0.8 }}
-  viewport={{ once: true }}
-  className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-6"
->
-  {data?.slice(0, 6).map((item, index) => (
     <motion.div
-      key={item._id}
-      initial={{ opacity: 0, y: 40 }}
+      initial={{ opacity: 0, y: 60 }}
       whileInView={{ opacity: 1, y: 0 }}
       transition={{
-        duration: 0.5,
-        delay: index * 0.15,
+        duration: 0.8,
+        ease: "easeOut",
       }}
       viewport={{ once: true }}
-      whileHover={{ y: -8 }}
+      className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-6"
     >
-      <Cardfacility data={item} />
+      {data.slice(0, 6).map((item, index) => (
+        <motion.div
+          key={item._id}
+          initial={{ opacity: 0, y: 40 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{
+            duration: 0.5,
+            delay: index * 0.15,
+          }}
+          viewport={{ once: true }}
+          whileHover={{
+            y: -8,
+            transition: { duration: 0.2 },
+          }}
+        >
+          <Cardfacility data={item} />
+        </motion.div>
+      ))}
     </motion.div>
-  ))}
-</motion.div>
-
-      </div>
-      <UpcomingEvents></UpcomingEvents>
-
-<WhyChooseUs></WhyChooseUs>
-      
-    </>
   );
-}
+};
+
+export default FeaturedFacilities;
