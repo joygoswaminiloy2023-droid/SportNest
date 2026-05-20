120f43de4a280ee56bba94eb771b0028085ac986
Joy Goswami <joygoswaminiloy2023@gmail.com>
2026-05-20 10:56:06 +0600

    all facility token genarate

--- src/app/All-Facilities/page.jsx
+++ src/app/All-Facilities/page.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import Allfacility from '../Components/Allfacility/Allfacility';
 
-
+import { auth } from '@/lib/auth';
+import { headers } from 'next/headers';
 
 const page = async () => {
-
+ const  {token}=await auth.api.getToken({
+     headers:await headers()
+ })
 
   const res = await fetch(`${process.env.NEXT_PUBLIC_BETTER_AUTH_URL}/facility`, {
     cache: "no-cache",
-    
+    headers: {
+     authorization: `Bearer ${token}`
+    },
   });
 
   const data = await res.json();
