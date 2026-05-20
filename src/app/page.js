4bc91b0f962ee2dfe5f2ddbc2975357dec33588d
Joy Goswami <joygoswaminiloy2023@gmail.com>
2026-05-20 10:35:41 +0600

    framer motion added issue fixed

--- src/app/page.js
+++ src/app/page.js
@@ -19,17 +19,14 @@ export default async function Home() {
         
         <h1 className="text-2xl font-bold text-center lg:text-left ml-10 ">Featured Facilities</h1>
         
-     <motion.div
+  <motion.div
   initial={{ opacity: 0, y: 60 }}
   whileInView={{ opacity: 1, y: 0 }}
-  transition={{
-    duration: 0.8,
-    ease: "easeOut",
-  }}
+  transition={{ duration: 0.8 }}
   viewport={{ once: true }}
   className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-6"
 >
-  {data.slice(0, 6).map((item, index) => (
+  {facilities?.slice(0, 6).map((item, index) => (
     <motion.div
       key={item._id}
       initial={{ opacity: 0, y: 40 }}
@@ -39,15 +36,13 @@ export default async function Home() {
         delay: index * 0.15,
       }}
       viewport={{ once: true }}
-      whileHover={{
-        y: -8,
-        transition: { duration: 0.2 },
-      }}
+      whileHover={{ y: -8 }}
     >
       <Cardfacility data={item} />
     </motion.div>
   ))}
 </motion.div>
+
       </div>
       <UpcomingEvents></UpcomingEvents>
 
